133ab12118794671981413040773012174998b7f
Theau BOIDOUX <boidouxtheau@sfr.fr>
2024-04-17 19:50:20 +0200

    page acceuil avec recherche youtuber fonctionelle

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -6,32 +6,35 @@ import { ApiYoutubeService } from '../api-youtube.service';
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent implements OnInit {
-  alderiateActivities: any[] = [];
-  alderiateVideos: any[] = [];
-  latestActivities: any[] = []; // Déclaration de la propriété latestActivities
+export class AccueilComponent {
+  searchQuery: string = '';
+  YoutuberName: string = '';
+  LatestsVideos : any[] = [];
+  Searched: boolean = false;
 
 
   constructor(private youtubeService: ApiYoutubeService) { }
 
-  ngOnInit(): void {
-    this.getAlderiateActivities();
-    this.getAlderiateVideos();
-  }
+  search() {
+    if (this.searchQuery.trim() === '') {
+      return; // Ne rien faire si la recherche est vide
+    }
+    this.YoutuberName = this.searchQuery;
 
-  getAlderiateActivities() {
-    this.youtubeService.getActivities('UCB3Vqxt5hVRKKWivG_OI4DA').subscribe((data: any) => {
-      this.alderiateActivities = data.items;
+    // Appeler le service pour rechercher le nom YouTube
+    this.youtubeService.getChannelId(this.searchQuery).subscribe((data: any) => {
+      if (data.pageInfo.totalResults != 0) {
+        const channelId = data.items[0].id; // Supposons que le premier résultat est le bon
+        // Appeler le service pour récupérer les dernières activités en utilisant le channelId
+        this.youtubeService.getLatestVideos(channelId,5).subscribe((videos: any) => {
+          // Émettre les dernières activités vers le composant parent (AppComponent)
+          this.LatestsVideos = videos.items;
+          this.Searched = true;
+        });
+      }else{
+        console.log('Aucun résultat trouvé');
+        this.Searched = false;
+      }
     });
-  }
-
-  getAlderiateVideos() {
-    this.youtubeService.getLatestVideos('UCB3Vqxt5hVRKKWivG_OI4DA', 5).subscribe((data: any) => {
-      this.alderiateVideos = data.items;
-    });
-  }
-  
-  onLatestActivitiesLoaded(activities: any[]) {
-    this.latestActivities = activities;
   }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,11 +10,9 @@ import { ApiYoutubeService } from '../api-youtube.service';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-  @Output() latestActivitiesLoaded = new EventEmitter<any>(); // Événement pour émettre les dernières activités
   @ViewChild(MatSidenav) sidenav!: MatSidenav; // Référence à MatSidenav
 
   public auth = inject(Auth);
-  searchQuery: string = '';
   isCollapsed = true; // Déclaration de la propriété isCollapsed
   isMobile = false; // Déclaration de la propriété isMobile
 
@@ -39,21 +37,6 @@ export class SidebarComponent {
       this.sidenav.open(); // On desktop/tablet, the menu can never be fully closed
       this.isCollapsed = !this.isCollapsed;
     }
-  }
-  search() {
-    if (this.searchQuery.trim() === '') {
-      return; // Ne rien faire si la recherche est vide
-    }
-
-    // Appeler le service pour rechercher le nom YouTube
-    this.youtubeService.getChannelId(this.searchQuery).subscribe((data: any) => {
-      const channelId = data.items[0].id; // Supposons que le premier résultat est le bon
-      // Appeler le service pour récupérer les dernières activités en utilisant le channelId
-      this.youtubeService.getLatestVideos(channelId).subscribe((videos: any) => {
-        // Émettre les dernières activités vers le composant parent (AppComponent)
-        this.latestActivitiesLoaded.emit(videos.items);
-      });
-    });
   }
 
   disconnect() {
